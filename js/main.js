23038e7e0f70df49d4aaa3075d125b66eae899ed
Nazariy <85013455+nazar2203@users.noreply.github.com>
2022-01-22 17:02:00 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var typed = new Typed(".auto-input", {
+    strings: ["NAZARIY SHYPKA", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop:true
+})
+let menu = document.querySelector('#menu-bar')
+let navbar = document.querySelector('.navbar')
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times')
+    navbar.classList.toggle('active')
+}
+window.onscroll = () =>{
+    menu.classList.remove('fa-times')
+    navbar.classList.remove('active')
+  
+}
