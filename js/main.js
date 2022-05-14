b3c1024da25862ae2c70be577707cbf150310ab9
Nazariy Shypka <85013455+NazariySh@users.noreply.github.com>
2022-05-14 17:43:59 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var typed = new Typed(".auto-input", {
+let typed = new Typed(".auto-input", {
     strings: ["NAZARIY SHYPKA", "Web Developer"],
     typeSpeed: 100,
     backSpeed: 100,
@@ -15,3 +15,184 @@ window.onscroll = () =>{
     navbar.classList.remove('active')
   
 }
+let audio = document.querySelector('#audio-visual');
+
+let wire1 = document.querySelector('.audio-wire-1');
+let wire2 = document.querySelector('.audio-wire-2');
+let wire3 = document.querySelector('.audio-wire-3');
+let wire4 = document.querySelector('.audio-wire-4');
+let wire5 = document.querySelector('.audio-wire-5');
+wire1.style.animationPlayState = 'paused';
+wire2.style.animationPlayState = 'paused';
+wire3.style.animationPlayState = 'paused';
+wire4.style.animationPlayState = 'paused';
+wire5.style.animationPlayState = 'paused';
+
+
+let audioMusic = ()=>{
+   
+    let i =0;
+  
+     
+    
+    audio.onclick = function(){
+       
+        if(i==0){
+            i=1;
+           
+            wire1.style.animationPlayState = 'running';
+            wire2.style.animationPlayState = 'running';
+            wire3.style.animationPlayState = 'running';
+            wire4.style.animationPlayState = 'running';
+            wire5.style.animationPlayState = 'running';
+            document.getElementById('player').play()
+    
+        
+        }else {
+          
+            wire1.style.animationPlayState = 'paused';
+            wire2.style.animationPlayState = 'paused';
+            wire3.style.animationPlayState = 'paused';
+            wire4.style.animationPlayState = 'paused';
+            wire5.style.animationPlayState = 'paused';
+            document.getElementById('player').pause()
+        i= 0;
+        
+      
+        }
+       
+    
+    };
+}
+audioMusic();
+
+
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height =window.innerHeight;
+let particlesArray;
+let mouse ={
+    x: null,
+    y: null,
+    radius: (canvas.height/80) * (canvas.width/80)
+}
+window.addEventListener('mousemove',
+      function(event){
+          mouse.x = event.x;
+          mouse.y = event.y;
+
+      }
+)
+
+
+class Particle {
+    constructor(x, y, directionX, directionY, size , color){
+        this.x = x;
+        this.y = y;
+        this.directionX = directionX;
+        this.directionY = directionY;
+        this.size = size;
+        this.color = color;
+
+    }
+    draw(){
+        ctx.beginPath();
+        ctx.arc(this.x, this.y , this.size, 0 , Math.PI * 2, false);
+        ctx.fillStyle = "#8C5523";
+        ctx.fill();
+
+
+
+    }
+    update() {
+        if (this.x > canvas.width || this.x < 0){
+            this.directionX = -this.directionX;
+
+        }
+        if( this.y > canvas.height || this.y <0){
+            this.directionY = -this.directionY
+        }
+
+
+
+
+        let dx = mouse.x - this.x;
+        let dy = mouse.y - this.y;
+        let distance = Math.sqrt(dx*dx + dy * dy);
+        if(distance < mouse.radius + this.size){
+            if(mouse.x < this.x && this.x < canvas.width - this.size * 10){
+                this.x += 10 ;
+            }
+            if(mouse.x > this.x && this.x > this.size *10){
+                this.x -= 10;
+
+            }
+            if(mouse.y < this.y && this.y < canvas.height - this.size *10){
+                this.y += 10;
+            }
+            if(mouse.y > this.y && this.y > this.size *10){
+                this.y -= 10;
+            }
+        }
+        this.x += this.directionX;
+        this.y += this.directionY;
+        this.draw();
+
+    }
+}
+
+
+function init(){
+    particlesArray = [];
+    let numberOfParticles = (canvas.height * canvas.width) / 9000;
+    for(let i =0; i < numberOfParticles ; i++){
+        let size = (Math.random() * 5) +1;
+        let x = (Math.random()* ((innerWidth - size * 2) - (size * 2)) + size * 2);
+        let y =  (Math.random()* ((innerHeight -size * 2) - (size * 2)) + size * 2)
+    let directionX = (Math.random() * 5) -2.5;
+    let directionY = (Math.random() * 5) -2.5;
+    let color = "#8C5523";
+    particlesArray.push(new Particle(x, y, directionX, directionY , size, color));
+    }
+}
+function connect(){
+    let opacityValue = 1;
+    for(let a = 0; a< particlesArray.length ; a++){
+        for(let b = a; b < particlesArray.length; b++){
+            let distance = ((particlesArray[a].x - particlesArray[b].x)
+             *(particlesArray[a].x - particlesArray[b].x))
+             +((particlesArray[a].y - particlesArray[b].y) * 
+             (particlesArray[a].y - particlesArray[b].y));
+             if(distance < (canvas.width/7) * (canvas.height/7) ){
+                 opacityValue = 1 -(distance/20000)
+                 ctx.strokeStyle = "rgba(140, 85, 31, "+ opacityValue+")";
+                 ctx.lineWidth =1;
+                 ctx.beginPath();
+                 ctx.moveTo(particlesArray[a].x, particlesArray[a].y);
+                 ctx.lineTo(particlesArray[b].x, particlesArray[b].y);
+                 ctx.stroke();
+             }
+        }
+    }
+}
+function animate(){
+    requestAnimationFrame(animate);
+    ctx.clearRect(0, 0 ,innerWidth, innerHeight)
+    for(let i=0; i < particlesArray.length; i++){
+        particlesArray[i].update();
+    }
+    connect();
+}
+window.addEventListener('resize', function(){
+canvas.width = this.innerWidth;
+canvas.height = this.innerHeight;
+mouse.radius = ((canvas.height/80) *(canvas.height/80))
+init();
+});
+window.addEventListener('mouseout', function(){
+    mouse.x = undefined;
+    mouse.x = undefined;
+})
+init();
+animate();
